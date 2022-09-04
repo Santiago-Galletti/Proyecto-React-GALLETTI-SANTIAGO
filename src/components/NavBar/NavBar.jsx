bf4a778306be4c189a2dcf497714bbc34ad360ac
Santiago Galletti <santiago.galletti@gmail.com>
2022-09-04 10:58:40 -0300

    Entrega final 1.2 correcion de navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./navbar.css";
 import CartWidget from "../CartWidget/CartWidget";
 
@@ -10,18 +10,18 @@ function NavBar(){
                 <Link to="/">
                     <img className="nav-logo" src="https://i.im.ge/2022/09/03/ObReu4.585f9333cb11b227491c3581.png" alt="logo"></img>
                 </Link>
-                <Link to="/">
-                    <li className="nav-link">Home</li>
-                </Link>
-                <Link to="/category/avengers">
-                    <li className="nav-link">Avengers</li>
-                </Link>
-                <Link to="/category/thor">
-                    <li className="nav-link">Thor</li>
-                </Link>
-                <Link to="/category/spiderman">
-                    <li className="nav-link">Spiderman</li>
-                </Link>
+                <NavLink to="/" className={({isActive}) => (isActive ? "active" : "noActive")} >
+                    <li>Home</li>
+                </NavLink>
+                <NavLink to="/category/avengers" className={({isActive}) => (isActive ? "active" : "noActive")}>
+                    <li>Avengers</li>
+                </NavLink>
+                <NavLink to="/category/thor" className={({isActive}) => (isActive ? "active" : "noActive")}>
+                    <li>Thor</li>
+                </NavLink>
+                <NavLink to="/category/spiderman" className={({isActive}) => (isActive ? "active" : "noActive")}>
+                    <li>Spiderman</li>
+                </NavLink>
                 <Link to="/cart">
                     <CartWidget />
                 </Link>
